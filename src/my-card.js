7b7bee9b5b777b140417674edb57f8b0490a38ec
Rex Kenyon <Rex@kenyon.us.com>
2025-02-13 12:12:48 -0500

    This is my first commit

--- src/my-card.js
+++ src/my-card.js
@@ -14,7 +14,10 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
-    this.title = "My card";
+    this.title = "Red Woods";
+    this.img = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Op2mehiUmXhnD1Mmb3p8gAHaEK%26pid%3DApi&f=1&ipt=e4feb207652abe7caee4ba7fafb4cc16cdafca9d3d3e8e7308717bf2cea55268&ipo=images";
+    this.description = "This is the california Red woods"
+    this.link = "hax.psu.edu"
   }
 
   static get styles() {
@@ -22,16 +25,71 @@ export class MyCard extends LitElement {
       :host {
         display: block;
       }
+      .cardlist {
+        display: flex;
+        width: 1200px;
+      }
+
+      .card {
+        background-color: black;
+        display: block;
+        padding: 8px;
+        margin: 8px;
+        width: 400px;
+        height: 400px;
+        border-radius: 20px;
+      }
+      .hax {
+        background-color: blue;
+      }
+      p {
+        color: white;
+        text-align: center;
+      }
+      h1 {
+        color: white;
+        text-align: center;
+      }
+      img {
+        width: 300px;
+        height: 200px;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+      }
+      .card-button {
+        padding: 10px;
+        color: black;
+        border-radius: 100px;
+        border-color: green;
+        border-width: 10px;
+        font-size: 16px;
+        font-weight: bold;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+      }
+
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+    <div class="card">
+      <h1>${this.title}</h1>
+      <img src="${this.img}"></img>
+      <p>${this.description}</p>
+      <button class="card-button"><a href="${this.link}">Click Me</a></button>
+    </div> 
+    `;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      img: {type: String},
+      description: {type: String},
+      link: {type: String},
     };
   }
 }
