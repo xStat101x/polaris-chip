f115d1a1eb6088a0b019a543224b2bf80591682d
Rex Kenyon <Rex@kenyon.us.com>
2025-05-06 22:36:22 -0400

    Hw 5 is done

--- src/my-card.js
+++ src/my-card.js
@@ -16,14 +16,27 @@ export class MyCard extends LitElement {
     super();
     this.title = "Red Woods";
     this.img = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Op2mehiUmXhnD1Mmb3p8gAHaEK%26pid%3DApi&f=1&ipt=e4feb207652abe7caee4ba7fafb4cc16cdafca9d3d3e8e7308717bf2cea55268&ipo=images";
-    this.description = "This is the california Red woods"
     this.link = "hax.psu.edu"
+    this.fancy = false;
   }
 
   static get styles() {
     return css`
       :host {
         display: block;
+        width: 400px;
+      }
+      :host([fancy]) {
+      display: block;
+      background-color: pink;
+      border: 2px solid fuchsia;
+      box-shadow: 10px 5px 5px red;
+      }
+      .fancy {
+        display: block;
+        background-color: pink;
+        border: 2px solid fuchsia;
+        box-shadow: 10px 5px 5px red;
       }
       .card {
         background-color: black;
@@ -93,9 +106,10 @@ export class MyCard extends LitElement {
   }
 
   // put this anywhere on the MyCard class; just above render() is probably good
+  // put this anywhere on the MyCard class; just above render() is probably good
   openChanged(e) {
     console.log(e.newState);
-    if (e.newState === "open") {
+    if (e.target.open) {
       this.fancy = true;
     }
     else {
@@ -107,12 +121,12 @@ export class MyCard extends LitElement {
     return html`
     <div class="card">
       <h1>${this.title}</h1>
-      <img src="${this.img}"></img>
+      <img src="${this.img}"/>
       <!-- put this in your render method where you had details -->
-      <details id='5' ?open="${this.fancy}" @toggle="${this.openChanged}">
+      <details ?open="${this.fancy}" @toggle="${this.openChanged}">
         <summary>Description</summary>
         <div>
-          ${this.description}
+          <slot></slot>
         </div>
       </details>
       <button class="card-button"><a href="${this.link}">Click Me</a></button>
@@ -122,10 +136,11 @@ export class MyCard extends LitElement {
 
   static get properties() {
     return {
+      ...super.properties,
       title: { type: String },
       img: {type: String},
-      description: {type: String},
       link: {type: String},
+      fancy: {type: Boolean, reflect: true},
     };
   }
 }
